59975b32692407ed411faea66a60501b5d0c9d1e
timpamungkas <timpamungkas@users.noreply.github.com>
2023-03-01 13:41:18 +0000

    chore(protobuf-go) : Auto-generated by Github Action

--- protogen/go/bank/bank_grpc.pb.go
+++ protogen/go/bank/bank_grpc.pb.go
@@ -18,86 +18,86 @@ import (
 // Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
-// HelloServiceClient is the client API for HelloService service.
+// BankServiceClient is the client API for BankService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type HelloServiceClient interface {
+type BankServiceClient interface {
 	GetCurrentBalance(ctx context.Context, in *CurrentBalanceRequest, opts ...grpc.CallOption) (*CurrentBalanceResponse, error)
 }
 
-type helloServiceClient struct {
+type bankServiceClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewHelloServiceClient(cc grpc.ClientConnInterface) HelloServiceClient {
-	return &helloServiceClient{cc}
+func NewBankServiceClient(cc grpc.ClientConnInterface) BankServiceClient {
+	return &bankServiceClient{cc}
 }
 
-func (c *helloServiceClient) GetCurrentBalance(ctx context.Context, in *CurrentBalanceRequest, opts ...grpc.CallOption) (*CurrentBalanceResponse, error) {
+func (c *bankServiceClient) GetCurrentBalance(ctx context.Context, in *CurrentBalanceRequest, opts ...grpc.CallOption) (*CurrentBalanceResponse, error) {
 	out := new(CurrentBalanceResponse)
-	err := c.cc.Invoke(ctx, "/bank.HelloService/GetCurrentBalance", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/bank.BankService/GetCurrentBalance", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// HelloServiceServer is the server API for HelloService service.
-// All implementations must embed UnimplementedHelloServiceServer
+// BankServiceServer is the server API for BankService service.
+// All implementations must embed UnimplementedBankServiceServer
 // for forward compatibility
-type HelloServiceServer interface {
+type BankServiceServer interface {
 	GetCurrentBalance(context.Context, *CurrentBalanceRequest) (*CurrentBalanceResponse, error)
-	mustEmbedUnimplementedHelloServiceServer()
+	mustEmbedUnimplementedBankServiceServer()
 }
 
-// UnimplementedHelloServiceServer must be embedded to have forward compatible implementations.
-type UnimplementedHelloServiceServer struct {
+// UnimplementedBankServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedBankServiceServer struct {
 }
 
-func (UnimplementedHelloServiceServer) GetCurrentBalance(context.Context, *CurrentBalanceRequest) (*CurrentBalanceResponse, error) {
+func (UnimplementedBankServiceServer) GetCurrentBalance(context.Context, *CurrentBalanceRequest) (*CurrentBalanceResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetCurrentBalance not implemented")
 }
-func (UnimplementedHelloServiceServer) mustEmbedUnimplementedHelloServiceServer() {}
+func (UnimplementedBankServiceServer) mustEmbedUnimplementedBankServiceServer() {}
 
-// UnsafeHelloServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to HelloServiceServer will
+// UnsafeBankServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to BankServiceServer will
 // result in compilation errors.
-type UnsafeHelloServiceServer interface {
-	mustEmbedUnimplementedHelloServiceServer()
+type UnsafeBankServiceServer interface {
+	mustEmbedUnimplementedBankServiceServer()
 }
 
-func RegisterHelloServiceServer(s grpc.ServiceRegistrar, srv HelloServiceServer) {
-	s.RegisterService(&HelloService_ServiceDesc, srv)
+func RegisterBankServiceServer(s grpc.ServiceRegistrar, srv BankServiceServer) {
+	s.RegisterService(&BankService_ServiceDesc, srv)
 }
 
-func _HelloService_GetCurrentBalance_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _BankService_GetCurrentBalance_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CurrentBalanceRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(HelloServiceServer).GetCurrentBalance(ctx, in)
+		return srv.(BankServiceServer).GetCurrentBalance(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/bank.HelloService/GetCurrentBalance",
+		FullMethod: "/bank.BankService/GetCurrentBalance",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(HelloServiceServer).GetCurrentBalance(ctx, req.(*CurrentBalanceRequest))
+		return srv.(BankServiceServer).GetCurrentBalance(ctx, req.(*CurrentBalanceRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// HelloService_ServiceDesc is the grpc.ServiceDesc for HelloService service.
+// BankService_ServiceDesc is the grpc.ServiceDesc for BankService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var HelloService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "bank.HelloService",
-	HandlerType: (*HelloServiceServer)(nil),
+var BankService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "bank.BankService",
+	HandlerType: (*BankServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "GetCurrentBalance",
-			Handler:    _HelloService_GetCurrentBalance_Handler,
+			Handler:    _BankService_GetCurrentBalance_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
